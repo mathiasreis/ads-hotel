1b8946b31eeb087f3410f266d3258bc89ff99275
Alexsander Reis <alexsander.arsilva@gmail.com>
2022-06-07 12:18:40 -0400

    Criação da classe Cliente no package model

---
+++ AdsHotel/src/model/Cliente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author alexr
+ */
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCliente(String nome, String cpf, String email, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
